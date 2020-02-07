b2855bfa730daa1eba3e302eecb75c688e34e6c2
Alan Vezina <alanv@labkey.com>
2020-02-06 17:01:50 -0800

    Example code for withQueryModel

---
+++ packages/components/src/stories/QueryModel.tsx
@@ -0,0 +1,53 @@
+import React, { PureComponent } from 'react';
+import { InjectedQueryModelProps, withQueryModel, WithQueryModelProps } from '../models/QueryModel';
+import { storiesOf } from '@storybook/react';
+
+interface ExampleProps extends InjectedQueryModelProps {
+    message: string,
+}
+
+interface ExampleState {
+    loading: boolean,
+    data: any,
+}
+
+class ExampleComponentImpl extends PureComponent<ExampleProps, ExampleState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            data: null,
+        };
+    }
+
+    render() {
+        const { queryModel } = this.props;
+        return <div>{this.props.message} - {queryModel.schemaName} - {queryModel.queryName} - {queryModel.viewName}</div>;
+    }
+}
+
+const config = {
+    id: 'myId',
+    schemaName: 'mySchema',
+    queryName: 'myQuery',
+    viewName: 'myView',
+};
+
+// Type safe, compiler will error when missing the props expected by MakeQueryModelProps or any other props from the
+// ExampleProps interface. Slightly annoying because you need to pass in the Props type again.
+const ExampleComponent = withQueryModel<ExampleProps>(ExampleComponentImpl);
+
+// In the case that you forget to specify the type of your wrapped component it will complain if you forget any props
+// from MakeQueryModelProps, but it has no clue if you forget anything from ExampleProps, and it lets you pass in any
+// props you want, whether your component uses them or not. As far as I know there is no way to require someone pass
+// a type to withQueryModel.
+const ExampleComponentTwo = withQueryModel(ExampleComponentImpl);
+
+storiesOf('QueryModel Examples', module)
+    .add('Type Safe withQueryModel', () => {
+        return <ExampleComponent queryConfig={config} message="Hello World" />;
+    })
+    .add('Not Type Safe withQueryModel', () => {
+        // Unfortunately this compiles, it should not.
+        return <ExampleComponentTwo queryConfig={config} />;
+    });
